37c88affb06fa370c23cc9de4c47f6a93b5a28c6
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2025-12-16 03:43:52 +0200

    sql query for data cleaning

---
+++ project/data-cleaning.sql
@@ -0,0 +1,20 @@
+USE StagingDatabase;
+
+CREATE TABLE dbo.extractedPricesClean(
+DateAvg DATE NOT NULL,
+PriceAvg DECIMAL(10,2) NOT NULL,
+DateMin DATE NOT NULL,
+PriceMin DECIMAL(10,2) NOT NULL
+);
+
+INSERT INTO dbo.extractedPricesClean(DateAvg, PriceAvg, DateMin, PriceMin)
+SELECT 
+TRY_CAST(REPLACE(DateAvg, '.','-') AS DATE),
+TRY_CAST(REPLACE(PriceAvg,',','.') AS DECIMAL(10,2)),
+TRY_CAST(REPLACE(DateMin, '.', '-') AS DATE),
+TRY_CAST(REPLACE(PriceMin, ',', '.') AS DECIMAL(10,2))
+FROM dbo.extractedPrices
+WHERE 
+ISDATE(DateAvg) = 1 
+AND TRY_CAST(REPLACE(PriceAvg,',','.') AS DECIMAL(10,2)) IS NOT NULL 
+AND TRY_CAST(REPLACE(PriceMin, ',', '.') AS DECIMAL(10,2)) IS NOT NULL;
